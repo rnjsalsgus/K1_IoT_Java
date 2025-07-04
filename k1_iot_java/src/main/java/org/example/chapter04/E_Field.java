76438a4b16244d422eb007007f70c6a1aea67ac8
Minion <54874601+rnjsalsgus@users.noreply.github.com>
2025-07-04 15:56:08 +0900

    Add files via upload

--- k1_iot_java/src/main/java/org/example/chapter04/E_Field.java
+++ k1_iot_java/src/main/java/org/example/chapter04/E_Field.java
@@ -30,9 +30,73 @@ package org.example.chapter04;
 // - 자동으로 초기화 X, 반드시 초기화 후 사용
 //      >> 초기화 없이 사용할 경우 예외!
 
+// cf) 변수명 - 동일한 스코프(scope, 영역) 내에서 같은 변수명 사용 X
+
+// cf) 클래스명 - 접근제한자에 따라 같은 이름의 클래스 사용 X
+// class CarClass {
+//
+//}
+
+// >> 접근제한자의 생략 - 기본값 default 설정(같은 패키지 안에서 사용 가능)
+// class Car {
+//
+//}
+
+class CarClass {
+    int a;
+//    int a;
+
+//    cf) 메서드명 - 동일한 스코프 내에서 같은 변수명 사용 가능
+    
+//    1. 필드(속성)
+//    1) 인스턴스 필드: 각 객체마다 다른 값을 가짐
+    String brand;
+    String color;
+    int speed;
+    
+//    2) 클래스(정적, static) 필드: 클래스의 모든 객체가 동일한 값을 가짐
+    static int tireNumber;
+    static int doorNumber;
+    
+//    2. 지역 변수
+    void accelerate (int increment) {
+//        speed 필드: 지역 변수 내에서 접근 가능
+        
+//        increment 지역 변수
+//        : 메서드 내에서만 유효
+        speed += increment; // speed = speed + increment
+    }
+}
 
 public class E_Field {
     public static void main(String[] args) {
+//        1) 인스턴스 필드 사용 방법
+//         객체명.필드명;
+
+//        2) 클래스 필드 사용 방법
+//         클래스명.필드명; || 객체명.필드명;
+//         >> 각 객체명으로도 호출 가능하지만, 클래스명 호출 방법을 권장!
+        System.out.println(CarClass.doorNumber); // 0
+        System.out.println(CarClass.tireNumber); // 0
+        
+//        클래스 필드값 변경
+        CarClass car1 = new CarClass();
+        CarClass car2 = new CarClass();
+
+//        cf) 예외, 오류 발생 X - 권장하지 않음
+//        car1.doorNumber = 4;
+//        System.out.println(car2.doorNumber);
+        System.out.println(CarClass.doorNumber);
+
+//        인스턴스 필드 사용
+        car1.brand = "kia";
+        car1.color = "black";
+        car1.accelerate(100);
+        car1.accelerate(100);
+        System.out.println(car1.speed);
+
+//        System.out.println(CarClass.brand); - 인스턴스 필드는 클래스로 호출 불가!
+
 
     }
 }
